5b1afeed23c9f96a07cd862600a1edb90abe9595
riitei <2590119197@qq.com>
2017-11-14 11:54:24 +0800

    練習報名系統

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>台中教育大學＿碩博班報名系統</title>
+</head>
+<body>
+<br>
+<form action="index.php" method="get" id="ir">
+    <span style="color: red">＊</span> 姓名：
+    <input type="text" id="name" size="10"><br>
+    <span style="color: red">&nbsp;&nbsp;&nbsp;</span> 年齡：
+    <input type="number" id="age" min="20"max="100" step="1" value="22"><br>
+    <span style="color: red">＊</span> 姓名：<input type="text" id="name" size="10"><br>
+    <span style="color: red">＊</span> 姓名：<input type="text" id="name" size="10"><br>
+    <span style="color: red">＊</span> 姓名：<input type="text" id="name" size="10"><br>
+    <span style="color: red">＊</span> 姓名：<input type="text" id="name" size="10"><br>
+    <span style="color: red">＊</span> 姓名：<input type="text" id="name" size="10"><br>
+    <span style="color: red">＊</span> 姓名：<input type="text" id="name" size="10"><br>
+    <span style="color: red">＊</span> 姓名：<input type="text" id="name" size="10"><br>
+    <input type="submit" value="送出">
+</form>
+</body>
+</html>
