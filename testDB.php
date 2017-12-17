267f02911725cb983ba3d5baeeab70a716e99348
riitei <2590119197@qq.com>
2017-12-17 23:57:31 +0800

    sql補上遺漏學校名單

--- testDB.php
+++ testDB.php
@@ -7,11 +7,19 @@
  */
 //include_once "autoload.php";
 include "DBconnect.php";
-//include 'Department.php';
+include 'Department.php';
 //include 'School.php';
-//$department = new Department();
+$department = new Department();
 //$school = new School();
-//$department= $department->department_search();
+$department= $department->department_search();
+
+$ntcu_department = new Department();
+foreach ($ntcu_department->department_search() as $value) {
+    echo $value['department'] . '<br>';
+    echo "  <option value=" . $value['department'] . ">" . $value['department'] . "</option>";
+}
+
+
 //echo 'ntcu <br>';
 //
 //foreach ($school->school_search() as $value){
@@ -19,19 +27,19 @@ include "DBconnect.php";
 //}
 
 
-$school_department = "SELECT school_department FROM Institute_Registration.school where school_name='國立高雄師範大學';";
-$departmen = DBconnect::connect()->query($school_department);
-
-$test = array();
-
-foreach ($departmen as $key => $value) {
-    //echo $value['school_department'].'<br>';
-    $test[$key]=$value['school_department'];
-}
-
-foreach ($test as $value) {
-    echo $value.'<br>';
-}
+//$school_department = "SELECT school_department FROM Institute_Registration.school where school_name='國立高雄師範大學';";
+//$departmen = DBconnect::connect()->query($school_department);
+//
+//$test = array();
+//
+//foreach ($departmen as $key => $value) {
+//    //echo $value['school_department'].'<br>';
+//    $test[$key]=$value['school_department'];
+//}
+//
+//foreach ($test as $value) {
+//    echo $value.'<br>';
+//}
 
 
 //
